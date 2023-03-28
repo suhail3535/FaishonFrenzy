3b11927c6fe3c8a0ab29bd12c75ff29fa3753da4
shagunsharma6677 <shagunsharma6677@gmail.com>
2023-03-29 03:33:53 +0530

    day-2 First

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import MainRoutes from "./Pages/MainRoutes/MainRoutes";
+import { SingleProduct } from "./Pages/SingleProduct/SingleProduct";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <MainRoutes />
+      <SingleProduct/>
       <Footer />
     </div>
   );
