bb693e22cf34f164e16c676b7af75b142e61def0
shagunsharma6677 <shagunsharma6677@gmail.com>
2023-03-29 05:11:57 +0530

    payment pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,16 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import MainRoutes from "./Pages/MainRoutes/MainRoutes";
-import { SingleProduct } from "./Pages/SingleProduct/SingleProduct";
+// import { SingleProduct } from "./Pages/SingleProduct/SingleProduct";
+import CartPage from "./Pages/CartPage/CartPage";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <MainRoutes />
-      <SingleProduct/>
+      {/* <SingleProduct/> */}
+      <CartPage/>
       <Footer />
     </div>
   );
