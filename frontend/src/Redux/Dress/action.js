f2cf771df07f5e5eff0ffa18e470662311aff5a6
Tushit99 <97578587+Tushit99@users.noreply.github.com>
2023-03-29 10:55:23 +0530

    redux

---
+++ frontend/src/Redux/Dress/action.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { DRESS_DATA_LOADING, DRESS_DATA_SUCCESS } from "./actionType";
+
+export const getDress = (param) => async (dispatch) => {
+  try {
+    dispatch({ type: DRESS_DATA_LOADING });
+    let res = await axios
+      .get("http://localhost:8080/dress", param)
+      .then((e) => {
+        dispatch({ type: DRESS_DATA_SUCCESS, payload: e.data });
+        return e.data;
+      });
+    console.log(res);
+  } catch (err) {
+    dispatch({ type: DRESS_DATA_ERROR });
+  }
+};
