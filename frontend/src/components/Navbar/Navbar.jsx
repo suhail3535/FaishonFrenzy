dc0befc702297288df32248431df74f605c1c46b
suhail3535 <khansohail015@gmail.com>
2023-03-30 15:10:28 +0530

    navtempand routes

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -42,7 +42,7 @@ import { Navigate } from "react-router-dom";
 import { FaUserAlt } from "react-icons/fa";
 import {FiSearch} from "react-icons/fi";
 import {SlHandbag} from "react-icons/sl" 
-
+import OnhoverProp from './Onhover';
 
 const Links = ['𝐵𝑒𝒻𝒶𝓈𝒽𝒾𝑜𝓃𝒶𝒷𝓁𝑒'];
 
@@ -86,95 +86,121 @@ export default function Navbar() {
 
   return (
     <>
-      <Image src={Source2}  alt={"Error"}/>
-      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4} borderBottom={"0.1px solid #b1b5be"} borderTop={"0.1px solid #b1b5be"}>
-        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+      {/* <BelowNavbar /> */}
+      <OnhoverProp />
+      <Image src={Source2} alt={"Error"} />
+      <Box
+        bg={useColorModeValue("gray.100", "gray.900")}
+        px={4}
+        borderBottom={"0.1px solid #b1b5be"}
+        borderTop={"0.1px solid #b1b5be"}
+      >
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
           <IconButton
-            size={'md'}
+            size={"md"}
             icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-            aria-label={'Open Menu'}
-            display={{ md: 'none' }}
+            aria-label={"Open Menu"}
+            display={{ md: "none" }}
             onClick={isOpen ? onClose : onOpen}
           />
-          <HStack spacing={8} alignItems={'center'}>
-            <Box pb={4} display={['none','none','none','block','block']}>
+          <HStack spacing={8} alignItems={"center"}>
+            <Box pb={4} display={["none", "none", "none", "block", "block"]}>
               <RouterLink to="/">
-            
-           <p style={{borderRight:"0.1px solid #b1b5be",padding:"10px",fontWeight:"bold",fontSize:"122%",borderTop:"3.5px solid #167a92",borderLeft:"0.1px solid #b1b5be"}}> 𝙵𝚊𝚒𝚜𝚑𝚘𝚗𝙵𝚛𝚎𝚗𝚣𝚢</p>   
+                <p
+                  style={{
+                    borderRight: "0.1px solid #b1b5be",
+                    padding: "10px",
+                    fontWeight: "bold",
+                    fontSize: "122%",
+                    borderTop: "3.5px solid #167a92",
+                    borderLeft: "0.1px solid #b1b5be",
+                  }}
+                >
+                  {" "}
+                  𝙵𝚊𝚒𝚜𝚑𝚘𝚗𝙵𝚛𝚎𝚗𝚣𝚢
+                </p>
               </RouterLink>
             </Box>
             <HStack
-             borderRight={"0.1px solid #b1b5be"}
-             
-             w={"200px"} height={"65px"}
-              as={'nav'}
+              borderRight={"0.1px solid #b1b5be"}
+              w={"200px"}
+              height={"65px"}
+              as={"nav"}
               spacing={4}
-              pb={4} display={['none','none','none','flex','block']}>
+              pb={4}
+              display={["none", "none", "none", "flex", "block"]}
+            >
               {Links.map((link) => (
                 <NavLink key={link}>{link}</NavLink>
               ))}
             </HStack>
           </HStack>
-         
-           <Flex w={"50%"}>
-           <input onChange={(e) => setInputData(e.target.value)} placeholder='Search FaishonFrenzy' style={{width: "100%",border:"1px solid grey"}} _focus={"none"} borderRadius={"none"} />
-           <Button _hover={"#167a92"} border={"2px solid #167a92"} borderRadius={"none"} backgroundColor={"#167a92"}  color={"white"}><FiSearch/></Button>
-           </Flex>
 
-           <Button onClick={toggleColorMode}>
-                {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+          <Flex w={"50%"}>
+            <input
+              onChange={(e) => setInputData(e.target.value)}
+              placeholder="Search FaishonFrenzy"
+              style={{ width: "100%", border: "1px solid grey" }}
+              _focus={"none"}
+              borderRadius={"none"}
+            />
+            <Button
+              _hover={"#167a92"}
+              border={"2px solid #167a92"}
+              borderRadius={"none"}
+              backgroundColor={"#167a92"}
+              color={"white"}
+            >
+              <FiSearch />
+            </Button>
+          </Flex>
+
+          <Button onClick={toggleColorMode}>
+            {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
           </Button>
-         
-         <RouterLink to={"/cart"}>
-       
-        <Box pb={4} display={['none','none','none','block','block']}><SlHandbag style={{height:"350px", width:"35px", color:"#167a92"}} /></Box> 
-         </RouterLink>
-          
-          <Flex  alignItems={'center'}>
-            <Menu  > 
-              
 
+          <RouterLink to={"/cart"}>
+            <Box pb={4} display={["none", "none", "none", "block", "block"]}>
+              <SlHandbag
+                style={{ height: "350px", width: "35px", color: "#167a92" }}
+              />
+            </Box>
+          </RouterLink>
+
+          <Flex alignItems={"center"}>
+            <Menu>
               <MenuButton
                 as={Button}
-                rounded={'full'}
-                variant={'link'}
-                cursor={'pointer'}
+                rounded={"full"}
+                variant={"link"}
+                cursor={"pointer"}
                 minW={0}
-                pb={4} display={['none','none','none','block','block']}
-                > 
-                
-            
-                
-                  <FaUserAlt marginLeft={"20px"} size={"25px"} /> 
+                pb={4}
+                display={["none", "none", "none", "block", "block"]}
+              >
+                <FaUserAlt marginLeft={"20px"} size={"25px"} />
               </MenuButton>
-              <MenuList >
+              <MenuList>
                 {/* <MenuItem> */}
                 <MenuItem>
-                   <RouterLink to="/login">
-
-                       Login
-                    
-                    </RouterLink>
-             </MenuItem>
-             <MenuItem >
-                  LogOut
-             </MenuItem>
+                  <RouterLink to="/login">Login</RouterLink>
+                </MenuItem>
+                <MenuItem>LogOut</MenuItem>
               </MenuList>
             </Menu>
           </Flex>
         </Flex>
 
         {isOpen ? (
-          <Box pb={4} display={['none','none','none','block','block']}>
-            <Stack  as={'nav'} spacing={4}>
+          <Box pb={4} display={["none", "none", "none", "block", "block"]}>
+            <Stack as={"nav"} spacing={4}>
               {Links.map((link) => (
-                <NavLink key={link} >{link}</NavLink>
+                <NavLink key={link}>{link}</NavLink>
               ))}
             </Stack>
           </Box>
         ) : null}
       </Box>
-      
     </>
   );
 }
