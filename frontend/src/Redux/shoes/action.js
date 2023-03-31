e13913606cf77331b53e58e70df1f61172631e5d
Tushit99 <97578587+Tushit99@users.noreply.github.com>
2023-03-31 22:49:16 +0530

    shoes Page

---
+++ frontend/src/Redux/shoes/action.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import {
+  SHOES_DATA_ERROR,
+  SHOES_DATA_LOADING,
+  SHOES_DATA_SUCCESS,
+} from "./actionType";
+
+export const getShoes = (param) => async (dispatch) => {
+  try {
+    dispatch({ type: SHOES_DATA_LOADING });
+    let res = await axios
+      .get("http://localhost:7700/shoes", param)
+      .then((e) => {
+        dispatch({ type: SHOES_DATA_SUCCESS, payload: e.data });
+        return e.data;
+      });
+    console.log(res);
+  } catch (err) {
+    dispatch({ type: SHOES_DATA_ERROR });
+  }
+};
