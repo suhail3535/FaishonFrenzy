7bfc08ec81cb8c116c7643cec1f6c4fe5564230b
suhail3535 <khansohail015@gmail.com>
2023-04-02 17:37:42 +0530

    update

--- frontend/src/Pages/MainRoutes/MainRoutes.jsx
+++ frontend/src/Pages/MainRoutes/MainRoutes.jsx
@@ -1,23 +1,19 @@
-
-import React from 'react'
-import HomePage from '../HomePage/HomePage'
-import { Route, Routes } from 'react-router-dom'
-import { SingleProduct } from '../SingleProduct/SingleProduct'
-import CartPage from '../CartPage/CartPage'
-import Admin from "../Admin/Admin"
-import Edit from '../Admin/Edit'
-import AuthPage from '../AuthPage/AuthPage'
-import Dresses from '../Dresses/Dresses'
-import ShippingAdresss from '../Shipping/ShippingAdress'
-import ShippingDetails from '../Shipping/ShippingDetails'
-import Dashboard from '../Admin/Dashboard'
-import Manage from '../Admin/Manage'
-import Shoes from '../Shoes/Shoes'
+import React from "react";
+import HomePage from "../HomePage/HomePage";
+import { Route, Routes } from "react-router-dom";
+import { SingleProduct } from "../SingleProduct/SingleProduct";
+import CartPage from "../CartPage/CartPage";
+import Admin from "../Admin/Admin";
+import Edit from "../Admin/Edit";
+import AuthPage from "../AuthPage/AuthPage";
+import Dresses from "../Dresses/Dresses";
+import ShippingAdresss from "../Shipping/ShippingAdress";
+import ShippingDetails from "../Shipping/ShippingDetails";
+import Dashboard from "../Admin/Dashboard";
+import Manage from "../Admin/Manage";
+import Shoes from "../Shoes/Shoes";
 import Clothing from "../Clothing/Clothing";
-import AdminLogin from '../AdminRegister/Adminlogin'
-
-
-
+import AdminLogin from "../AdminRegister/Adminlogin";
 
 const MainRoutes = () => {
   return (
@@ -25,21 +21,19 @@ const MainRoutes = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/cart" element={<CartPage />} />
+        <Route path="/dress" element={<Dresses />} />
         <Route path="/admin" element={<Admin />} />
-        <Route path="/edit/:id" element={<Edit />} />
         <Route path="/authpage" element={<AuthPage />} />
+        <Route path="/shoes" element={<Shoes />} />
         <Route path="/ship" element={<ShippingAdresss />} />
-        <Route path="/:name/:id" element={<SingleProduct />} />
+        <Route path="/cloths" element={<Clothing />} />
         <Route path="/shipping_address" element={<ShippingDetails />} />
-        <Route path="/admin_Dashboard" element={<Dashboard />} />
         <Route path="/manage" element={<Manage />} />
         <Route path="/adminlogin" element={<AdminLogin />} />
         <Route path="/top" element={<top />} />
-
-        <Route path="/dress" element={<Dresses />} />
-
-        <Route path="/shoes" element={<Shoes />} />
-        <Route path="/cloths" element={<Clothing />} />
+        <Route path="/admin_Dashboard" element={<Dashboard />} />
+        <Route path="/edit/:id" element={<Edit />} />
+        <Route path="/:name/:id" element={<SingleProduct />} />
       </Routes>
     </div>
   );
