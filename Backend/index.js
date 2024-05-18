bcf9a01ad2795d131ab089ebf22be98bcb7561a3
suhail khan <khansohail015@gmail.com>
2024-05-18 07:43:54 +0530

    update code

--- Backend/index.js
+++ Backend/index.js
@@ -35,7 +35,7 @@ app.use("/adminsdetails", adminRouter); //this route is for admin login and deta
 
 app.use(
   cors({
-    origin: "https://trainee-assignment-dashboard.vercel.app",
+    origin: "https://faishon-frenzy.vercel.app",
     // origin: "http://localhost:5173",
     methods: ["POST", "GET","UPDATE","DELETE"],
     credentials: true
